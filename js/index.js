8e7de84369b8ec30de97349f564d9e7b9d4447f1
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-06-28 19:23:14 +0300

    adding todolist v1.0

---
+++ js/index.js
@@ -0,0 +1,130 @@
+const selectors = {
+  todoInput: document.querySelector(".todo-input"),
+  todoButton: document.querySelector(".todo-button"),
+  todoList: document.querySelector(".todo-list"),
+  filterOption: document.querySelector(".filter-todo"),
+};
+
+selectors.todoButton.addEventListener("click", addToDo);
+selectors.todoList.addEventListener("click", deleteCheck);
+selectors.filterOption.addEventListener("click", filterTodo);
+
+function addToDo(event) {
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create li
+  const newToDo = document.createElement("li");
+  newToDo.innerText = selectors.todoInput.value;
+  newToDo.classList.add("todo-item");
+  todoDiv.appendChild(newToDo);
+  //add todo to localstorage
+  saveLocalTodos(selectors.todoInput.value);
+  //markbtn
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //trashbtn
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //append to li
+  selectors.todoList.appendChild(todoDiv);
+  selectors.todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = selectors.todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    const newToDo = document.createElement("li");
+    newToDo.innerText = todo;
+    newToDo.classList.add("todo-item");
+    todoDiv.appendChild(newToDo);
+    //markbtn
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //trashbtn
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //append to li
+    selectors.todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
